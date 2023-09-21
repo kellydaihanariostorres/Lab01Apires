3e4ee78b28b6e870fcdd156fe2b032e576a6c37d
kelly <114171338+kellydaihanariostorres@users.noreply.github.com>
2023-09-21 11:43:33 -0500

    cap 10 working with delete requests

--- Lab01Apirestl/Program.cs
+++ Lab01Apirestl/Program.cs
@@ -17,7 +17,7 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
-
+///
 builder.Services.Configure<ApiBehaviorOptions>(options => {
     options.SuppressModelStateInvalidFilter = true;
 });
